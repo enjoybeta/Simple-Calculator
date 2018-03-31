1ec1058cb2cc01e13d4d56634ab273c71b93fd82
enjoybeta <goujiashun@gmail.com>
2018-03-31 14:43:40 -0400

    v4.1.1
    remove things imported from simple-tool
    new style outlook

--- app/src/main/kotlin/com/simplemobiletools/calculator/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/activities/MainActivity.kt
@@ -5,36 +5,28 @@ import android.support.v4.app.FragmentManager
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
 import com.simplemobiletools.calculator.BuildConfig
 import com.simplemobiletools.calculator.*
-import com.simplemobiletools.calculator.extensions.config
 import com.simplemobiletools.calculator.extensions.updateViewColors
 import com.simplemobiletools.calculator.fragments.OneFragment
 import com.simplemobiletools.calculator.helpers.*
-import com.simplemobiletools.commons.extensions.*
-import com.simplemobiletools.commons.helpers.LICENSE_AUTOFITTEXTVIEW
-import com.simplemobiletools.commons.helpers.LICENSE_ESPRESSO
-import com.simplemobiletools.commons.helpers.LICENSE_KOTLIN
-import com.simplemobiletools.commons.helpers.LICENSE_ROBOLECTRIC
 import kotlinx.android.synthetic.main.activity_main.*
 import me.grantland.widget.AutofitHelper
 
 
-class MainActivity : SimpleActivity() {
-    private var storedTextColor = 0
+class MainActivity : AppCompatActivity() {
     private var vibrateOnButtonPress = true
-    private var storedUseEnglish = false
 
     private var calc: Calculator = Calculator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        appLaunched()
 
         //setListeners()
         val adapter = ViewPagerAdapter(supportFragmentManager)
@@ -44,8 +36,6 @@ class MainActivity : SimpleActivity() {
 
         AutofitHelper.create(result)
         AutofitHelper.create(formula)
-        storeStateVariables()
-        updateViewColors(calculator_holder, config.textColor)
     }
 
     internal inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
@@ -152,81 +142,32 @@ class MainActivity : SimpleActivity() {
         result.text = calc.trimDecimal(calc.getResultText())
     }
 
-    override fun onResume() {
-        super.onResume()
-        if (storedUseEnglish != config.useEnglish) {
-            restartActivity()
-            return
-        }
-
-        if (storedTextColor != config.textColor) {
-            updateViewColors(calculator_holder, config.textColor)
-        }
-
-        if (config.preventPhoneFromSleeping) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        }
-
-        vibrateOnButtonPress = config.vibrateOnButtonPress
-    }
-
-    override fun onPause() {
-        super.onPause()
-        storeStateVariables()
-        if (config.preventPhoneFromSleeping) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        }
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.settings -> launchSettings()
-            R.id.about -> startAboutActivity(
-                    R.string.app_name,
-                    LICENSE_KOTLIN or LICENSE_AUTOFITTEXTVIEW or LICENSE_ROBOLECTRIC or LICENSE_ESPRESSO,
-                    BuildConfig.VERSION_NAME
-            )
-            else -> return super.onOptionsItemSelected(item)
-        }
-        return true
-    }
-
-    private fun storeStateVariables() {
-        config.apply {
-            storedTextColor = textColor
-            storedUseEnglish = useEnglish
-        }
-    }
-
     private fun doClickAnimation(view: View) {
         if (vibrateOnButtonPress) {
-            view.performHapticFeedback()
+            view.performHapticFeedback(1)
         }
-    }
-
-    private fun launchSettings() {
-        startActivity(Intent(applicationContext, SettingsActivity::class.java))
     }
 
     //private fun getButtonIds() = arrayOf(btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9)
 
-    private fun copyToClipboard(copyResult: Boolean): Boolean {
-        var value = formula.value
-        if (copyResult) {
-            value = result.value
-        }
-
-        return if (value.isEmpty()) {
-            false
-        } else {
-            copyToClipboard(value)
-            true
-        }
-    }
+//    private fun copyToClipboard(copyResult: Boolean): Boolean {
+//        var value = formula.text
+//        if (copyResult) {
+//            value = result.text
+//        }
+//
+//        return if (value.isEmpty()) {
+//            false
+//        } else {
+//            copyToClipboard(value)
+//            true
+//        }
+//    }
 
 }
