f5bda2c5e053b6c0d4a0cad526aae6d426a0001d
enjoybeta <goujiashun@gmail.com>
2018-06-01 23:24:43 -0400

    v4.6.1
    better implementation to handle clicks in FragmentButtons class, less overhead and better readability

--- app/src/main/kotlin/com/simplemobiletools/calculator/fragments/FragmentButtons.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/fragments/FragmentButtons.kt
@@ -16,31 +16,6 @@ import kotlinx.android.synthetic.main.fragment_buttons.*
 class FragmentButtons : Fragment() {
     private lateinit var buttonconfig: ButtonConfig
 
-    private lateinit var buttonObj11: Buttons
-    private lateinit var buttonObj12: Buttons
-    private lateinit var buttonObj13: Buttons
-    private lateinit var buttonObj14: Buttons
-
-    private lateinit var buttonObj21: Buttons
-    private lateinit var buttonObj22: Buttons
-    private lateinit var buttonObj23: Buttons
-    private lateinit var buttonObj24: Buttons
-
-    private lateinit var buttonObj31: Buttons
-    private lateinit var buttonObj32: Buttons
-    private lateinit var buttonObj33: Buttons
-    private lateinit var buttonObj34: Buttons
-
-    private lateinit var buttonObj41: Buttons
-    private lateinit var buttonObj42: Buttons
-    private lateinit var buttonObj43: Buttons
-    private lateinit var buttonObj44: Buttons
-
-    private lateinit var buttonObj51: Buttons
-    private lateinit var buttonObj52: Buttons
-    private lateinit var buttonObj53: Buttons
-    private lateinit var buttonObj54: Buttons
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_buttons, container, false)
     }
@@ -54,26 +29,6 @@ class FragmentButtons : Fragment() {
 
     fun initButtonConfig(bc: ButtonConfig) {
         buttonconfig = bc
-        buttonObj11 = ButtonFactory.getButtonInstance(bc.btn11)
-        buttonObj12 = ButtonFactory.getButtonInstance(bc.btn12)
-        buttonObj13 = ButtonFactory.getButtonInstance(bc.btn13)
-        buttonObj14 = ButtonFactory.getButtonInstance(bc.btn14)
-        buttonObj21 = ButtonFactory.getButtonInstance(bc.btn21)
-        buttonObj22 = ButtonFactory.getButtonInstance(bc.btn22)
-        buttonObj23 = ButtonFactory.getButtonInstance(bc.btn23)
-        buttonObj24 = ButtonFactory.getButtonInstance(bc.btn24)
-        buttonObj31 = ButtonFactory.getButtonInstance(bc.btn31)
-        buttonObj32 = ButtonFactory.getButtonInstance(bc.btn32)
-        buttonObj33 = ButtonFactory.getButtonInstance(bc.btn33)
-        buttonObj34 = ButtonFactory.getButtonInstance(bc.btn34)
-        buttonObj41 = ButtonFactory.getButtonInstance(bc.btn41)
-        buttonObj42 = ButtonFactory.getButtonInstance(bc.btn42)
-        buttonObj43 = ButtonFactory.getButtonInstance(bc.btn43)
-        buttonObj44 = ButtonFactory.getButtonInstance(bc.btn44)
-        buttonObj51 = ButtonFactory.getButtonInstance(bc.btn51)
-        buttonObj52 = ButtonFactory.getButtonInstance(bc.btn52)
-        buttonObj53 = ButtonFactory.getButtonInstance(bc.btn53)
-        buttonObj54 = ButtonFactory.getButtonInstance(bc.btn54)
     }
 
     private fun initButtonText(){
@@ -106,87 +61,86 @@ class FragmentButtons : Fragment() {
     private fun setClickListeners() {
         //row1
         btn_11.setOnClickListener {
-            buttonObj11.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn11).handleClick()
             doClickAnimation(it)
         }
         btn_12.setOnClickListener {
-            buttonObj12.handleClick()
-            doClickAnimation(it)
+            ButtonFactory.getButtonInstance(buttonconfig.btn12).handleClick()
         }
         btn_13.setOnClickListener {
-            buttonObj13.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn13).handleClick()
             doClickAnimation(it)
         }
         btn_14.setOnClickListener {
-            buttonObj14.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn14).handleClick()
             doClickAnimation(it)
         }
         //row2
         btn_21.setOnClickListener {
-            buttonObj21.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn21).handleClick()
             doClickAnimation(it)
         }
         btn_22.setOnClickListener {
-            buttonObj22.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn22).handleClick()
             doClickAnimation(it)
         }
         btn_23.setOnClickListener {
-            buttonObj23.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn23).handleClick()
             doClickAnimation(it)
         }
         btn_24.setOnClickListener {
-            buttonObj24.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn24).handleClick()
             doClickAnimation(it)
         }
         //row3
         btn_31.setOnClickListener {
-            buttonObj31.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn31).handleClick()
             doClickAnimation(it)
         }
         btn_32.setOnClickListener {
-            buttonObj32.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn32).handleClick()
             doClickAnimation(it)
         }
         btn_33.setOnClickListener {
-            buttonObj33.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn33).handleClick()
             doClickAnimation(it)
         }
         btn_34.setOnClickListener {
-            buttonObj34.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn34).handleClick()
             doClickAnimation(it)
         }
         //row 4
         btn_41.setOnClickListener {
-            buttonObj41.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn41).handleClick()
             doClickAnimation(it)
         }
         btn_42.setOnClickListener {
-            buttonObj42.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn42).handleClick()
             doClickAnimation(it)
         }
         btn_43.setOnClickListener {
-            buttonObj43.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn43).handleClick()
             doClickAnimation(it)
         }
         btn_44.setOnClickListener {
-            buttonObj44.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn44).handleClick()
             doClickAnimation(it)
         }
         //row 5
         btn_51.setOnClickListener {
-            buttonObj51.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn51).handleClick()
             doClickAnimation(it)
         }
         btn_52.setOnClickListener {
-            buttonObj52.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn52).handleClick()
             doClickAnimation(it)
         }
         btn_53.setOnClickListener {
-            buttonObj53.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn53).handleClick()
             doClickAnimation(it)
         }
         btn_54.setOnClickListener {
-            buttonObj54.handleClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn54).handleClick()
             doClickAnimation(it)
         }
     }
@@ -194,87 +148,87 @@ class FragmentButtons : Fragment() {
     private fun setLongClickListeners() {
         //row1
         btn_11.setOnLongClickListener {
-            buttonObj11.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn11).handleLongClick()
             true
         }
         btn_12.setOnLongClickListener {
-            buttonObj12.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn12).handleLongClick()
             true
         }
         btn_13.setOnLongClickListener {
-            buttonObj13.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn13).handleLongClick()
             true
         }
         btn_14.setOnLongClickListener {
-            buttonObj14.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn14).handleLongClick()
             true
         }
         //row2
         btn_21.setOnLongClickListener {
-            buttonObj21.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn21).handleLongClick()
             true
         }
         btn_22.setOnLongClickListener {
-            buttonObj22.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn22).handleLongClick()
             true
         }
         btn_23.setOnLongClickListener {
-            buttonObj23.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn23).handleLongClick()
             true
         }
         btn_24.setOnLongClickListener {
-            buttonObj24.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn24).handleLongClick()
             true
         }
         //row3
         btn_31.setOnLongClickListener {
-            buttonObj31.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn31).handleLongClick()
             true
         }
         btn_32.setOnLongClickListener {
-            buttonObj32.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn32).handleLongClick()
             true
         }
         btn_33.setOnLongClickListener {
-            buttonObj33.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn33).handleLongClick()
             true
         }
         btn_34.setOnLongClickListener {
-            buttonObj34.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn34).handleLongClick()
             true
         }
         //row 4
         btn_41.setOnLongClickListener {
-            buttonObj41.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn41).handleLongClick()
             true
         }
         btn_42.setOnLongClickListener {
-            buttonObj42.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn42).handleLongClick()
             true
         }
         btn_43.setOnLongClickListener {
-            buttonObj43.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn43).handleLongClick()
             true
         }
         btn_44.setOnLongClickListener {
-            buttonObj44.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn44).handleLongClick()
             true
         }
         //row 5
         btn_51.setOnLongClickListener {
-            buttonObj51.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn51).handleLongClick()
             true
         }
         btn_52.setOnLongClickListener {
-            buttonObj52.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn52).handleLongClick()
             true
         }
         btn_53.setOnLongClickListener {
-            buttonObj53.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn53).handleLongClick()
             true
         }
         btn_54.setOnLongClickListener {
-            buttonObj54.handleLongClick()
+            ButtonFactory.getButtonInstance(buttonconfig.btn54).handleLongClick()
             true
         }
     }
